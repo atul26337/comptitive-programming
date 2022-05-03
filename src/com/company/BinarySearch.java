94d8746e30d89363b15b6c4ee5b3b361bd8c1d2b
atulkumar <atul.kr@fplabs.tech>
2022-05-03 19:14:13 +0530

    binary search

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -1,28 +1,28 @@
-//package com.company;
-//
-//public class BinarySearch {
-//
-//    public int search(int arr[] ,int traget){
-//        int left =0,right=arr.length-1;
-//        while (left<=right){
-//            int mid=(left+right)/2;
-//            if(arr[mid]==traget){
-//                return mid;
-//            }
-//            if(traget<arr[mid]){
-//                right=mid-1;
-//            }else {
-//                left=mid+1;
-//            }
-//        }
-//        return -1;
-//    }
-//
-//
-//    public static void  main(String[] args){
-//        int[] arr={-1,0,3,5,9,12};
-//        BinarySearch binarySearch=new BinarySearch();
-//        int result =
-//        System.out.print("result "+result);
-//    }
-//}
+package com.company;
+
+public class BinarySearch {
+
+    public int search(int[] nums, int target){
+        int left =0,right=nums.length-1;
+        while (left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(target<nums[mid]){
+                right=mid-1;
+            }else {
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void  main(String[] args){
+        int[] arr={1,3};
+        BinarySearch binarySearch=new BinarySearch();
+        int result =binarySearch.search(arr,0);
+        System.out.print("result "+result);
+    }
+}
---
+++ src/com/company/SearchInsertPosition.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0 ;
+        int end = nums.length -1 ;
+        int mid = start + (end -start)/2;
+
+        while(start <= end){
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] != target){
+                if(nums[mid]+1 == target && nums[mid] - 1 == target){
+                    return mid;
+                }
+            }
+
+            if(nums[mid] <= target){
+                start = mid + 1;
+            }
+            if(nums[mid]>target){
+                end = mid - 1;
+            }
+
+            mid = start + (end - start)/2;
+        }
+
+        return mid;
+    }
+    public static void main(String[] args){
+        int arr[]={1,3,5,6};
+        SearchInsertPosition searchInsertPosition=new SearchInsertPosition();
+        int result =searchInsertPosition.searchInsert(arr,8);
+        System.out.print("result "+result);
+    }
+}
